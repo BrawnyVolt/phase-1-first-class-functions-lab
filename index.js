6b333f23c588d5e58ab887153a43f8aac7cd1e6c
Steven Brock <shyfay0@gmail.com>
2021-08-31 17:45:17 -0700

    commit tree ba4e8902effa30426c86c9e5292ad9f46f4b90f7

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arrayOfDrivers){
+  let tempArray = arrayOfDrivers.slice(0,2)
+  return tempArray
+}
+
+function returnLastTwoDrivers(arrayOfDrivers){
+  let tempArray = arrayOfDrivers.slice(-2)
+  return tempArray
+}
+// function selectingDrivers(){
+//   let newArray = [returnFirstTwoDrivers, returnLastTwoDrivers]
+//   return newArray
+// }
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+ return function fareTaker(fare){return num * fare}
+}
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+
+
+function selectDifferentDrivers(arrayOfDrivers, func){
+if(func === returnFirstTwoDrivers){
+return returnFirstTwoDrivers(arrayOfDrivers);
+} else {return returnLastTwoDrivers(arrayOfDrivers)}
+  
+}
+  // return chooser(func)
+//   selectDifferentDrivers(drivers, returnFirstTwoDrivers))
+
+
+
+
+//   if first run returnFirstTwoDrivers
+// function 
+// else run second drives
+// let something = result
+// return something
+// }
+
+//     selectingDrivers
+//       3) has the `returnFirstTwoDrivers` function to as its first element
+//       4) has the `returnLastTwoDrivers` function to as its last element
+//       5) allows us to invoke either function from the array
+//     createFareMultiplier()
+//       6) returns a function
+//       7) should multiply a given value using the created multiplier
+//     fareDoubler()
+//       8) is a function
+//       9) doubles fares
+//     fareTripler()
+//       10) is a function
+//       11) triples fares
+//     selectDifferentDrivers(arrayOfDrivers, function)
+//       12) returns the first two drivers when passed returnFirstTwoDrivers() as the second argument
+//       13) returns the last two drivers when passed returnLastTwoDrivers() as the second argument
